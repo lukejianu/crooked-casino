2ebacfe2c799d56cdbd59294c6ba9627ba42b3a8
lukejianu <71729102+lukejianu@users.noreply.github.com>
2022-12-07 10:13:42 -0500

    small comment

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -1,4 +1,4 @@
-# Some set up for the application 
+# Some set up for the application
 
 from flask import Flask
 from flaskext.mysql import MySQL
@@ -6,28 +6,31 @@ from flaskext.mysql import MySQL
 # create a MySQL object that we will use in other parts of the API
 db = MySQL()
 
+
 def create_app():
     app = Flask(__name__)
-    
-    # secret key that will be used for securely signing the session 
-    # cookie and can be used for any other security related needs by 
+
+    # secret key that will be used for securely signing the session
+    # cookie and can be used for any other security related needs by
     # extensions or your application
     app.config['SECRET_KEY'] = 'someCrazyS3cR3T!Key.!'
 
-    # these are for the DB object to be able to connect to MySQL. 
+    # these are for the DB object to be able to connect to MySQL.
     app.config['MYSQL_DATABASE_USER'] = 'webapp'
-    app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_password.txt').readline()
+    app.config['MYSQL_DATABASE_PASSWORD'] = open(
+        '/secrets/db_password.txt').readline()
     app.config['MYSQL_DATABASE_HOST'] = 'db'
-    app.config['MYSQL_DATABASE_PORT'] = 3306
-    app.config['MYSQL_DATABASE_DB'] = 'database1'  # Change this to your DB name
+    app.config['MYSQL_DATABASE_PORT'] = 3306  # SHOULD BE 3200 - LUKE
+    # Change this to your DB name
+    app.config['MYSQL_DATABASE_DB'] = 'database1'
 
-    # Initialize the database object with the settings above. 
+    # Initialize the database object with the settings above.
     db.init_app(app)
-    
+
     # Import the various routes
     from src.views import views
     from src.customers.customers import customers
-    from src.products.products  import products
+    from src.products.products import products
 
     # Register the routes that we just imported so they can be properly handled
     app.register_blueprint(views,       url_prefix='/classic')
