47b49b254368bfe1170eb1601beb6790bebbfa7d
lukejianu <71729102+lukejianu@users.noreply.github.com>
2022-12-09 15:13:44 -0500

    players page working

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -28,13 +28,9 @@ def create_app():
     db.init_app(app)
 
     # Import the various routes
-    from src.views import views
-    from src.customers.customers import customers
-    from src.products.products import products
+    from src.appsmith.appsmith import appsmith
 
     # Register the routes that we just imported so they can be properly handled
-    app.register_blueprint(views,       url_prefix='/classic')
-    app.register_blueprint(customers,   url_prefix='/classic')
-    app.register_blueprint(products,    url_prefix='/classic')
+    app.register_blueprint(appsmith,    url_prefix='/app')
 
     return app
---
+++ flask-app/src/appsmith/appsmith.py
@@ -0,0 +1,56 @@
+from flask import Blueprint, request, jsonify, make_response
+import json
+from src import db
+
+
+appsmith = Blueprint('appsmith', __name__)
+
+# Player Page: Adding chips
+
+# Getting players to populate a dropdown.
+@appsmith.route('/players', methods=['GET'])
+def get_players():
+    cursor = db.get_db().cursor()
+    cursor.execute('select firstName as label, playerId as value from Player')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Get the portfolio of the player that corresponds to the given playerId.
+@appsmith.route('/portfolio/<playerId>', methods=['GET'])
+def get_portfolio(playerId):
+    cursor = db.get_db().cursor()
+    # playerId = request.headers["playerId"]
+    cursor.execute(
+        f'select total_chips_value, num_1, num_5, num_10, num_25, num_100 from Player NATURAL JOIN Portfolio WHERE playerId = {playerId}')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Add a post 
+@appsmith.route('/portfolio/<playerId>', methods=['POST'])
+def update_portfolio(playerId):
+    cursor = db.get_db().cursor()
+    # playerId = request.headers["playerId"]
+    num1_to_add = request.headers["num_1"]
+    num5_to_add = request.headers["num_5"]
+    num10_to_add = request.headers["num_10"]
+    num25_to_add = request.headers["num_25"]
+    num100_to_add = request.headers["num_100"]
+    cursor.execute(
+        f'''UPDATE Portfolio SET num_1 = num_1 + {num1_to_add}, num_5 = num_5 + {num5_to_add}, num_10 = num_10 + {num10_to_add}, num_25 = num_25 + {num25_to_add}, 
+        num_100 = num_100 + {num100_to_add}, total_chips_value = num_1 + num_5 * 5 + num_10 * 10 + num_25 * 25 + num_100 * 100 WHERE playerId = {playerId};''')
+    db.get_db().commit()
+    return "success"
