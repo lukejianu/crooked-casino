80241e3e618dbe839f873d17b9be75fce6f9a7ed
Suraj Ramchandran <30753067+Suraj-Ram@users.noreply.github.com>
2022-12-13 10:17:03 -0500

    Added some GET endpoints for employee management. Retrieves employees and their boss and average dealer data.

--- flask-app/src/admin/admin.py
+++ flask-app/src/admin/admin.py
@@ -33,3 +33,89 @@ def get_chart():
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+@admin.route('/employees', methods=['GET'])
+def get_employees():
+    """
+    Employee management view, where admins can see who each employee reports and their
+    dealer stats, namely, friendliness, corruptness and scumminess.
+    """
+    QUERY = """
+        SELECT d.firstName, d.lastName, d.friendliness, d.corruptness, d.scumminess, A.role, A.firstName as Reports_To from Dealer d
+        JOIN PokerTable PT ON d.tableId = PT.tableId
+        JOIN AdminTableBridge ATB ON PT.tableId = ATB.tableId
+        JOIN Admin A ON ATB.adminId = A.adminId;
+    """
+    cursor = db.get_db().cursor()
+    cursor.execute(QUERY)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+@admin.route('/dealer-friendliness', methods=['GET'])
+def get_avg_friendliness():
+    """
+    Get the average friendliness across all dealers.
+    """
+    QUERY = """
+        SELECT AVG(friendliness) as Avg_Friendliness FROM Dealer;
+    """
+    cursor = db.get_db().cursor()
+    cursor.execute(QUERY)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+@admin.route('/dealer-corruptness', methods=['GET'])
+def get_avg_corruptness():
+    """
+    Get the average corruptness across all dealers.
+    """
+    QUERY = """
+        SELECT AVG(corruptness) as Avg_Corruptness FROM Dealer;
+    """
+    cursor = db.get_db().cursor()
+    cursor.execute(QUERY)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+@admin.route('/dealer-scumminess', methods=['GET'])
+def get_avg_scumminess():
+    """
+    Get the average scumminess across all dealers.
+    """
+    QUERY = """
+        SELECT AVG(scumminess) as Avg_Scumminess FROM Dealer;
+    """
+    cursor = db.get_db().cursor()
+    cursor.execute(QUERY)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
